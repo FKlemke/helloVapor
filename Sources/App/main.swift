8a9f5dbecbc76352e80fac09dfd36f6c2cddcdc1
Felix E. C. Klemke <me@felixklemke.com>
2017-03-15 23:07:09 +0100

    added some roots

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -8,6 +8,26 @@ drop.get { req in
     ])
 }
 
+drop.get("helloFelix") {reg in
+    return "Hello Felix!"
+}
+
+drop.get("hello", Int.self) {reg, userID in
+    return "Hello Felix! this it your \(userID)"
+}
+
+drop.get("friends") { req in
+    return try JSON(node: ["friends": [
+        ["name":"Sven", "nationality by heart": "where the kivis grow"],
+        ["name":"gregor", "nationality by heart": "shakes & fitches"],
+        ["name":"dustin", "nationality by heart": "RUST"],
+        ["name":"jakob", "nationality by heart": "RICH JEW"],
+        ["name":"max", "nationality by heart": "Belgian Banana Republic"],
+        ["name":"dominik", "nationality by heart": "Zigeuner"],
+        ]
+        ])
+}
+
 drop.resource("posts", PostController())
 
 drop.run()
